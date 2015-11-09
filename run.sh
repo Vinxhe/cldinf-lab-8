196095af56e1863679dc8742bb76ee1daa3d8cef
Ueli Bosshard <ueli@lilo.ch>
2015-11-09 13:20:51 +0000

    fix in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" == "3" ] ; then
-  sudo mn --custom clos3.py --topo clos,leaf=3,spine=3
+  sudo mn --custom clos3.py --topo clos3,leaf=3,spine=3
 else
   sudo mn --custom clos.py --topo clos,leaf=3,spine=3
 fi
