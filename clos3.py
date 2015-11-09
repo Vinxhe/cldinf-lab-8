c9e44a936894c31f06a66db4e565106b7a1f9b83
Ueli Bosshard <ueli@lilo.ch>
2015-11-09 13:20:41 +0000

    initial commit of clos3.py

---
+++ clos3.py
@@ -0,0 +1,24 @@
+from mininet.topo import Topo
+
+class clos3(Topo):
+
+    def __init__(self, leaf, spine):
+        print(leaf)
+        print(spine)
+
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Add hosts and switches
+        leftHost = self.addHost( 'h1' )
+        rightHost = self.addHost( 'h2' )
+        leftSwitch = self.addSwitch( 's3' )
+        rightSwitch = self.addSwitch( 's4' )
+
+        # Add links
+        self.addLink( leftHost, leftSwitch )
+        self.addLink( leftSwitch, rightSwitch )
+        self.addLink( rightSwitch, rightHost )
+
+
+topos = { 'clos3': clos3 }
