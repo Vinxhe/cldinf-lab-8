196ef1a3b377414fb391d897646c23349b6b9c37
Felix Morgner <felix.morgner@gmail.com>
2015-11-09 13:22:23 +0000

    Add setup stubs

--- clos.py
+++ clos.py
@@ -3,7 +3,20 @@ from mininet.topo import Topo
 class clos(Topo):
 
     def __init__(self, leaf, spine):
-        print(leaf)
-        print(spine)
+        Topo.__init__(self)
+
+        spineDevs = self.create_spine(spine)
+        leafDevs = self.create_leaf(leaf)
+
+        self.hookup(spineDevs, leafDevs)
+
+    def create_spine(self, nofDevices):
+        return []
+
+    def create_leaf(self, nofDevices):
+        return []
+
+    def hookup(self, spineDevices, leafDevices):
+        pass
 
 topos = { 'clos': clos }
